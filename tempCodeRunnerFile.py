614aa4995f5af14bb815dc82f6d49732fd21a92d
Yann Lucas <101208372+Y4nnLS@users.noreply.github.com>
2025-04-28 19:55:50 -0300

    remove: temp code

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-def aplicar_harris(imagem, blockSize, ksize, k, nome_saida):
-    harris = cv2.cornerHarris(imagem, blockSize=blockSize, ksize=ksize, k=k)
-    harris = cv2.dilate(harris, None)
-    imagem_harris = imagem.copy()
-    imagem_harris[harris > 0.01 * harris.max()] = 255
-    cv2.imwrite(nome_saida, imagem_harris)
